67de6187b3e92ffc48e2d5964f5930d15c55e000
Yujun Liu <yujun.liu@vanderbilt.edu>
2018-03-06 21:09:19 -0800

    add log out

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -27,6 +27,12 @@
         dark to='register'>
           Sign Up
       </v-btn>
+      <v-btn
+        v-if="$store.state.isUserLoggedIn"
+        flat
+        dark @click='logout'>
+          Log Out
+      </v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
@@ -36,6 +42,13 @@ export default {
   methods: {
     navigateTo (route) {
       this.$router.push(route)
+    },
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: 'root'
+      })
     }
   }
 }
